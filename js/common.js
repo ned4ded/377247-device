a5c871df7e5b31a5943d3c6e1522c2603bed2849
ned4ded <wtf.czabe@gmail.com>
2017-05-17 16:52:52 +0300

    JS map + sprite

--- js/common.js
+++ js/common.js
@@ -1,3 +1,5 @@
+// WRITE US FORM
+
 var link = document.querySelector(".info__contacts .info__btn");
 
 var popup = document.querySelector(".write-us-form");
@@ -20,6 +22,31 @@ window.addEventListener("keydown", function(event) {
 if (event.keyCode === 27) {
   if (popup.classList.contains("write-us-form--active")) {
     popup.classList.remove("write-us-form--active");
+    }
+  }
+});
+
+// MAP
+
+var mapOpen = document.querySelector(".info__map");
+
+var mapPopup = document.querySelector(".info .info__map-popup");
+var mapClose = mapPopup.querySelector(".close-btn");
+
+mapOpen.addEventListener("click", function(event) {
+  event.preventDefault();
+  mapPopup.classList.add("info__map-popup--active");
+});
+
+mapClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  mapPopup.classList.remove("info__map-popup--active");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (mapPopup.classList.contains("info__map-popup--active")) {
+      mapPopup.classList.remove("info__map-popup--active");
     }
   }
 });
